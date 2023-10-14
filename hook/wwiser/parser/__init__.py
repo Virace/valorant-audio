2573abc74cff4cd77f745532abe4cc9b7b7c74d3
Virace <Virace@aliyun.com>
2023-10-15 00:34:33 +0800

    init

---
+++ hook/wwiser/parser/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author  : Virace
+# @Email   : Virace@aliyun.com
+# @Site    : x-item.com
+# @Software: Pycharm
+# @Create  : 2023/10/14 22:51
+# @Update  : 2023/10/14 22:51
+# @Detail  : 
