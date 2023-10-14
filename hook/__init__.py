601af036975758f491233ad91eddf74a2500ccc0
Virace <Virace@aliyun.com>
2023-10-14 16:34:06 +0800

    准备对wwiser进行修改

---
+++ hook/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author  : Virace
+# @Email   : Virace@aliyun.com
+# @Site    : x-item.com
+# @Software: Pycharm
+# @Create  : 2023/10/14 16:32
+# @Update  : 2023/10/14 16:32
+# @Detail  : 
