4f499af8d6106b5c75ef63fd011798a7b615a7ce
Virace <Virace@aliyun.com>
2023-10-14 16:33:29 +0800

    文件夹~ 创建！

---
+++ config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Author  : Virace
+# @Email   : Virace@aliyun.com
+# @Site    : x-item.com
+# @Software: Pycharm
+# @Create  : 2023/10/14 14:26
+# @Update  : 2023/10/14 14:29
+# @Detail  : 配置文件
+
+
+PAKS_PATH = ''
+AES_KEY = ''
+
+LOCALIZATION = 'zh-CN'
+
+# 前缀
+PACKAGE_PREFIX = 'ShooterGame/Content/WwiseAudio'
---
+++ main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author  : Virace
+# @Email   : Virace@aliyun.com
+# @Site    : x-item.com
+# @Software: Pycharm
+# @Create  : 2023/10/13 22:38
+# @Update  : 2023/10/13 22:38
+# @Detail  : 
+
