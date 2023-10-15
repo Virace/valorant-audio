ba91ae0ff1c1e1c30992c110111e5e7b80d25a4c
Virace <Virace@aliyun.com>
2023-10-15 20:21:44 +0800

    todo

--- main.py
+++ main.py
@@ -4,6 +4,9 @@
 # @Site    : x-item.com
 # @Software: Pycharm
 # @Create  : 2023/10/13 22:38
-# @Update  : 2023/10/13 22:38
+# @Update  : 2023/10/15 20:21
 # @Detail  : 
 
+# todo: 1. 先取出{LOCALIZATION}_Audio-WindowsClient.pak，存放于临时目录
+# todo: 2. 挂载pak文件
+# todo: 3. 遍历挂载目录，解析Event并取出wem
