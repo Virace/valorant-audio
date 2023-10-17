fbc88d5c05c64aa17eb33d50b128785ef12dd54e
Virace <Virace@aliyun.com>
2023-10-18 00:11:11 +0800

    bs4解析xml; 生成哈希表

--- main.py
+++ main.py
@@ -4,19 +4,20 @@
 # @Site    : x-item.com
 # @Software: Pycharm
 # @Create  : 2023/10/13 22:38
-# @Update  : 2023/10/16 23:24
+# @Update  : 2023/10/17 22:09
 # @Detail  : 
 
 import gc
+import json
 import os
 import shutil
-import xml.etree.ElementTree as ET
 
 from UE4Parse.Assets.Objects.FGuid import FGuid
 from UE4Parse.Encryption import FAESKey
 from UE4Parse.PakFile.PakObjects.FPakEntry import FPakEntry
 from UE4Parse.Provider import DefaultFileProvider
 from UE4Parse.Versions import EUEVersion, VersionContainer
+from bs4 import BeautifulSoup
 from loguru import logger
 
 from Utils import common
@@ -26,7 +27,7 @@ from hook.wwiser.viewer import wdumper
 
 
 # 1. 先取出{LOCALIZATION}_Audio-WindowsClient.pak，存放于临时目录
-# todo: 2. 挂载pak文件
+# 2. 挂载pak文件
 # todo: 3. 遍历挂载目录，解析Event并取出wem
 
 
@@ -58,7 +59,7 @@ class ValorantAudio:
         :return:
         """
         self.game_path = game_path
-        logger.debug(f'游戏目录: {self.game_path}')
+        logger.info(f'游戏目录: {self.game_path}')
         self.out_path = out_path
         self.game_version = get_valorant_version(
             os.path.join(self.game_path, 'ShooterGame/Binaries/Win64/VALORANT-Win64-Shipping.exe'))
@@ -79,6 +80,8 @@ class ValorantAudio:
         common.makedirs(self.PAKS_PATH)
         common.makedirs(self.HASH_PATH)
         common.makedirs(self.AUDIO_PATH)
+
+        self.audio_hash_file = os.path.join(self.HASH_PATH, 'data.json')
 
         self.pak_file = os.path.join(self.PAKS_PATH, f'{self.localization}_Audio-WindowsClient.pak')
 
@@ -108,6 +111,19 @@ class ValorantAudio:
             raise FileNotFoundError(
                 f'游戏目录下不存在{self.localization}_Audio-WindowsClient.pak，检查游戏目录是否正确')
 
+    @classmethod
+    def _get_file(cls, file: FPakEntry, raw=False):
+        """
+        获取文件内容
+        :param file:
+        :param raw:
+        :return:
+        """
+        if raw:
+            return file.get_data().base_stream
+        else:
+            return file.get_data().read()
+
     def mount_paks(self):
         """
         挂载pak文件, 返回文件列表
@@ -126,33 +142,75 @@ class ValorantAudio:
         # 因为目录中只有一个paks文件，所以取第一个
         return self.provider.files.Storage[0].files
 
-    def get_audio_ids_from_bnk(self, bnk: FPakEntry):
+    def get_audio_ids(self, bnk: FPakEntry):
         """
         获取音频id
         :return:
         """
-        # 后缀名不为bnk则报错
-        if bnk.Name.split('.')[-1] != 'bnk':
-            raise TypeError(f'文件{bnk.Name}不是bnk文件')
 
+        # if isinstance(bnk, bytes):
+        #
+        #     parser.parse_bank(name, raw=BytesIO(bnk))
+        # else:
+        #     # 后缀名不为bnk则报错
+        #     if bnk.Name.split('.')[-1] != 'bnk':
+        #         raise TypeError(f'文件{bnk.Name}不是bnk文件')
+        #     # parse_bank 第一个参数如果从内存直接读取则可以给虚假值， 但只能从dumper中调用get_xml_raw，其他任何write_x 函数都会造成不可预估错误
+        #     parser.parse_bank(bnk.Name, raw=bnk.get_data().base_stream)
         parser = wparser.Parser()
-
-        # parse_bank 第一个参数如果从内存直接读取则可以给虚假值， 但只能从dumper中调用get_xml_raw，其他任何write_x 函数都会造成不可预估错误
-        parser.parse_bank(bnk.Name, raw=bnk.get_data().base_stream)
+        parser.parse_bank(bnk.Name, raw=self._get_file(bnk, True))
         banks = parser.get_banks()
 
         dumper = wdumper.DumpPrinter(banks, 'none', '')
         data = dumper.get_xml_raw()
-        # if 'VO' not in bnk.Name:
-        #     with open(os.path.join(self.TEMP_PATH, f'{os.path.basename(bnk.Name)}.xml'), 'w+', encoding='utf-8') as f:
-        #         f.write(data)
-        # return
-        root = ET.fromstring(data)
-        # 寻找name为AkMediaInformation的所有子节点
+
         ids = []
-        for item in root.findall("./object[@name='HircChunk']/list/object[@name='CAkSound']"
-                                 "/object[@name='SoundInitialValues']"):
-            for sound in item.findall('*[@name="AkBankSourceData"]/*[@name="AkMediaInformation"]/*[@name="sourceID"]'):
-                ids.append(sound.get('value'))
+        soup = BeautifulSoup(data, features="lxml")
+        sounds = soup.find_all('object', {'name': 'CAkSound'})
+        for item in sounds:
+            bank_data = item.find('object', {'name': 'AkBankSourceData'})
+            stream_type = bank_data.find('field', {'name': 'StreamType'})
+            sid = item.find('field', {'name': 'sourceID'})
+
+            # if stream_type.attrs['value'] != '0':
+            #     logger.debug(f'添加{sid.attrs["value"]} - {stream_type.attrs["value"]}')
+            #     ids.append(sid.attrs['value'])
+            # else:
+            #     logger.debug(f'跳过{sid.attrs["value"]}，因为stream_type为{stream_type.attrs["value"]}')
+            ids.append(sid.attrs['value'])
 
         return ids
+
+    def get_audio_hash(self):
+        """
+        获取音频hash
+        """
+        result = dict()
+        files = self.mount_paks()
+
+        for name, file in files.items():
+
+            if 'Event' in name:
+
+                # Play_VO_Rift_LastRoundofHalfasAttackers
+                # Play_HURM_Multikill_Gekko
+                _, _type, _item, *_event = name.split('_')
+
+                if _type not in result:
+                    result[_type] = dict()
+
+                if _item not in result[_type]:
+                    result[_type][_item] = dict()
+                _event = '_'.join(_event)
+
+                # logger.debug(f'解析{name}')
+                ids = self.get_audio_ids(file)
+                logger.debug(f'解析{os.path.basename(name)}，共{len(ids)}个音频')
+                if len(ids) == 0:
+                    logger.warning(f'解析{os.path.basename(name)}，未找到音频')
+                result[_type][_item][_event] = ids
+
+        with open(self.audio_hash_file, 'w+', encoding='utf-8') as f:
+            json.dump(result, f, ensure_ascii=False, indent=4)
+
+        return result
